d1de06926fa5bc786539a3cdbe881dda908011cc
Jacob Dent <murkymicrobe88@gmail.com>
2025-02-24 16:45:18 -0500

    Added a subroutine that checks if user input is valid

--- src/bubble_sort.f90
+++ src/bubble_sort.f90
@@ -1,3 +1,25 @@
+! Subroutine that checks to make sure the user has inputed the proper data
+subroutine check_input(n)
+
+    integer, intent(inout) :: n
+    integer :: iunit, ios
+
+    iunit = 5
+    
+    do while (.true.)
+        read(iunit, *, iostat = ios) n
+
+        if ((ios /= 0) .or. (n < 0) ) then
+            print *, "Please input a valid integer"
+        else
+            exit
+        end if
+    end do
+end subroutine check_input
+
+
+
+
 program bubblesort
     implicit none
 
@@ -11,13 +33,16 @@ program bubblesort
 
     ! Ask how long the array will be
     print *, "How many numbers would you like to sort? "
-    read(*,*) length
+    call check_input(length)
     allocate(numbers(length))
+
+    ! Inform the user of the allowed inputs
+    print *, "Pleae input only positive integers"
 
     ! Ask to input variables
     do i = 1, length
         print *, "Please input the next number"
-        read(*,*) numbers(i)
+        call check_input(numbers(i))
     end do
 
     do i = 0, length - 1
