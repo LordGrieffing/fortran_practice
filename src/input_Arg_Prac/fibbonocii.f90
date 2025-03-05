a9a4a7239029e06121cc90e0cf3422c7ac0dff1b
Jacob Dent <murkymicrobe88@gmail.com>
2025-03-05 16:27:51 -0500

    working with input arguments. But my fibbonocii program for some reason can't take certain integers

---
+++ src/input_Arg_Prac/fibbonocii.f90
@@ -0,0 +1,58 @@
+recursive function fibbonocii(n) result (fibnum)
+    integer :: n, fibnum
+
+    if(n == 0) then
+        fibnum = 0
+    else if (n == 1) then
+        fibnum = 1
+    else
+        fibnum = fibbonocii(n-1) + fibbonocii(n-2)
+    end if
+end function fibbonocii
+  
+subroutine check_arg(n, arg_test)
+
+    logical, intent(inout) :: arg_test
+    integer, intent(inout) :: n
+    integer :: ios
+
+    if ((ios /= 0) .or. (n < 0) ) then
+        print *, "Please input a valid integer"
+        arg_test = .false.
+    else
+        arg_test = .true.
+    end if
+end subroutine check_arg
+
+
+program Main
+
+    ! Declare variables
+    logical :: arg_test
+    integer :: n, finalfib, fibbonocii
+    integer :: num_args, i, ios
+    character(len=12), dimension(:), allocatable :: args
+
+    ! Build argument array
+    num_args = command_argument_count()
+    allocate(args(num_args))
+
+    ! input arguments into agrument array
+    do i = 1, num_args
+        call get_command_argument(i, args(i))
+    end do
+
+    ! Check for valid arguments
+    read(args(1),*, iostat = ios) n
+    call check_arg(n, arg_test)
+
+    ! Perform fibbonocii or end program
+    if (arg_test) then
+        finalfib = fibbonocii(n)
+        print *, finalfib
+    else
+        print *, "Incorrect argument input closing program"
+    end if
+
+end program Main
+
